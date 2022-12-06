f954a9488e4141334015774b29ca8fb1ec7d51f2
JerryTZF <tzfforyou@163.com>
2022-12-06 13:46:40 +0800

    add: 项目骨架

---
+++ src/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm
+ * Time: 2022/12/6 13:30
+ * Author: JerryTian<tzfforyou@163.com>
+ * File: test.php
+ * Desc:
+ */
+
+namespace Biaodianyun\sdk;
+
+class Test
+{
+    public function test(): array
+    {
+        return [];
+    }
+}
---
+++ test/Test.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__. '/../vendor/autoload.php';
+
+use Biaodianyun\sdk\Test;
+
+$obj = new Test();
+var_dump($obj->test());
