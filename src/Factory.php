ec71df93ab81cf5d9973308825cd2ed00c877d3d
JerryTZF <tzfforyou@163.com>
2022-12-06 18:23:21 +0800

    add: 项目骨架

---
+++ src/Factory.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm
+ * Time: 2022/12/6 13:51
+ * Author: JerryTian<tzfforyou@163.com>
+ * File: Factory.php
+ * Desc:
+ */
+
+namespace Biaodianyun\Sdk;
+
+class Factory
+{
+}
